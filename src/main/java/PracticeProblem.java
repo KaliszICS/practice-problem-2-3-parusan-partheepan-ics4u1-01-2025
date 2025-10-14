bb84c5f20134b47681ad64c9db5bdaf3f428e9c6
parusan-partheepan-ics4u1-01-2025 <parusan.partheepan@student.tdsb.on.ca>
2025-10-14 21:46:43 +0000

    make changes

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,41 @@
 public class PracticeProblem {
-
-	public static void main(String args[]) {
-
+public static void main(String args[]) {
 	}
 
 	public static void q1() {
 		//Write question 1 code here
+        // Test the bubble sort
+        String[] words = {"banana", "Apple", "cherry", "date", "apricot"};
+        
+        System.out.println("Before sorting:");
+        for (String word : words) {
+            System.out.print(word + " ");
+        }
+        
+        bubbleSortString(words);
+        
+        System.out.println("\nAfter sorting:");
+        for (String word : words) {
+            System.out.print(word + " ");
+        }
+        // Output: Apple apricot banana cherry date
+    }
+    
+    public static void bubbleSortString(String[] strings) {
+        for (int i = 0; i < strings.length - 1; i++) {
+            for (int j = 0; j < strings.length - 1 - i; j++) {
+                // Compare ignoring uppercase/lowercase
+                if (strings[j].compareToIgnoreCase(strings[j + 1]) > 0) {
+                    // Swap the two strings
+                    String temp = strings[j];
+                    strings[j] = strings[j + 1];
+                    strings[j + 1] = temp;
+                }
+            }
+        }
+    }
+
 	}
 
-	public static void q2() {
-		//Write question 2 code here
-	}
 
-	public static void q3() {
-		//Write question 3 code here
-	}
 
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
-}
